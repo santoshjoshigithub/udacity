218867022434a783e993368848a6e0d08340b8e7
Santosh Joshi <kec.santosh.joshi@gmail.com>
2020-12-30 17:50:06 +0800

    updated many files

--- data_engineering/01_data_modelling_sql/project/README.md
+++ data_engineering/01_data_modelling_sql/project/README.md
@@ -11,6 +11,28 @@ This project can be divided into 2 main modules, which we will see in details:
 - In the star schema we have 1 fact table and 4 dimension tables.
 - The fact table, songplays, is connected with all the other dimension table. It has few keys and other junk dimensions and it could be extended to have different measures e.g. duration of the songs into it.
 - There are 4 dimension tables viz: users, songs, artists and time.
-- Following is the start schema diagram which dpecits the data model, tables and their relationship.
+- Following is the star schema diagram which dpecits the data model, tables and their relationship.
 
-<img src="https://github.com/santoshjoshigithub/udacity/blob/master/data_engineering/01_data_modelling_sql/images/relational_model.png" width="1000" height="500">
+<img src="https://github.com/santoshjoshigithub/udacity/blob/master/data_engineering/01_data_modelling_sql/images/relational_model.png" width="1000	" height="500">
+
+### The Data Pipeline:
+- The data pipeline is built using Python modules.
+- There are basically 3 main modules to set up the pipeline viz: create_tables.py, etl.py and sql_queries. There are 2 note book files which are being used for analysis and testing. They are etl.ipynb and test.ipynb.
+    - create_tables.py - This file basically creates a connection to the database, creates and drops the table.
+    - sql_queries.py - This module is being used by the other 2 modules. This is where all the SQL queries has been written.
+    - etl.py - This module basically drives the ETL, it consumes data from the source, transforms the data and loads it in the respective tables. It uses the other 2 modules to achieve it.
+- Some data quality/integrity checks has been applied e.g. Primary key is defined for each table and checks are implemented while insertion so that duplicates records are ignored and pipeline doesn't terminate with errors.
+    
+### Miscellaneous:
+- All the JSON files are stored in the Data Folder.
+- etl.ipynb file is used to do analysis of the data, transformation etc. The code written in this file is later used by etl.py file.
+- test.ipynb file is used to test the results in the respective fact and dimension tables.
+- There is an additional image folder which is used to store *.png file for project diagrams.
+
+# High Level Steps:
+- Go to the launcher in the Jupyter lab of Udacity project enviroment.
+- Open Terminal and run create_tables.py (>python create_tables.py and then hit run)
+- Please note, run create_tables.py file whenever you start data loading, this will drop and recreate all the tables again.
+- Sometimes you may encounter errors while running create_tables.py, it probably coz of the existing connection open, then go to any of the notebook file and restart it, this will reset the connection.
+- To load data from the JSON files, runetl.py file from the terminal.
+- For verification of the data, use test.ipynb file and see if data has been loaded or not.
--- data_engineering/01_data_modelling_sql/project/sql_queries.py
+++ data_engineering/01_data_modelling_sql/project/sql_queries.py
@@ -9,53 +9,145 @@ time_table_drop = "drop table if exists time"
 # CREATE TABLES
 
 songplay_table_create = ("""
-create table if not exists songplays (songplay_id int, start_time time , user_id int, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar)
+    create table if not exists songplays 
+        (
+            songplay_id int primary key, 
+            start_time time , 
+            user_id int, 
+            level varchar, 
+            song_id varchar, 
+            artist_id varchar, 
+            session_id int, 
+            location varchar, 
+            user_agent varchar
+        )
 """)
 
 user_table_create = ("""
-create table if not exists users(user_id int, first_name varchar, last_name varchar, gender varchar, level varchar)
+    create table if not exists users
+    (
+        user_id int primary key, 
+        first_name varchar, 
+        last_name varchar, 
+        gender varchar, 
+        level varchar
+    )
 """)
 
 song_table_create = ("""
-create table if not exists songs(song_id varchar, title varchar, artist_id varchar, year int, duration float)
+    create table if not exists songs
+    (
+        song_id varchar primary key, 
+        title varchar, 
+        artist_id varchar, 
+        year int, 
+        duration float
+    )
 """)
 
 artist_table_create = ("""
-create table if not exists artists(artist_id varchar, name varchar, location varchar, latitude varchar, longitude varchar)
+    create table if not exists artists
+    (
+        artist_id varchar primary key, 
+        name varchar, 
+        location varchar, 
+        latitude varchar, 
+        longitude varchar
+    )
 """)
 
 time_table_create = ("""
-create table if not exists time(start_time time, hour int, day int, week int, month int, year int, weekday int)
+    create table if not exists time
+    (
+        start_time time primary key, 
+        hour int, 
+        day int, 
+        week int,
+        month int,
+        year int,
+        weekday int)
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-insert into songplays(songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    insert into songplays
+        (
+            songplay_id, 
+            start_time, 
+            user_id, 
+            level,
+            song_id,
+            artist_id,
+            session_id,
+            location,
+            user_agent
+        ) 
+    values(%s,%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT (songplay_id) DO NOTHING;
 """)
 
 user_table_insert = ("""
-insert into users(user_id, first_name, last_name, gender, level) values  (%s, %s, %s, %s, %s)
-
+    insert into users
+    (
+        user_id, 
+        first_name,
+        last_name,
+        gender,
+        level
+    ) 
+    values (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING;
 """)
 
 song_table_insert = ("""
-insert into songs(song_id, title, artist_id, year, duration) values (%s, %s, %s, %s, %s)
+    insert into songs
+    (
+        song_id,
+        title,\
+        artist_id,
+        year,
+        duration
+    ) values (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
-insert into artists(artist_id, name, location, latitude, longitude) values(%s,%s,%s,%s,%s)
+    insert into artists
+    (
+        artist_id, 
+        name,
+        location,
+        latitude,
+        longitude
+    ) values(%s,%s,%s,%s,%s) ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 
 time_table_insert = ("""
-insert into time(start_time, hour, day, week, month, year, weekday) values (%s,%s,%s,%s,%s,%s,%s)
+    insert into time
+    (
+        start_time, 
+        hour,
+        day,
+        week,
+        month,
+        year,
+        weekday
+    ) values (%s,%s,%s,%s,%s,%s,%s) ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
-select distinct s.song_id, a.artist_id from songs as s left outer join artists as a on s.artist_id = a.artist_id where s.title = %s and a.name = %s and s.duration = %s 
+    select 
+        distinct 
+            s.song_id, 
+            a.artist_id 
+    from 
+        songs as s 
+        left outer join artists as a on s.artist_id = a.artist_id 
+    where 
+        s.title = %s and 
+        a.name = %s and 
+        s.duration = %s 
 """)
 
 # QUERY LISTS
